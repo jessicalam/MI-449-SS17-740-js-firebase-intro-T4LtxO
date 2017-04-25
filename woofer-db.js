a562964d5614123cc4025b4a0962932409a9d31d
jessicalam <j.lam@mail.com>
2017-04-25 13:21:44 -0400

    changed some stuff in the woof file

--- woofer-db.js
+++ woofer-db.js
@@ -1,19 +1,19 @@
 // Initialize Firebase
 var config = {
-  apiKey: 'AIzaSyBH0WBIAdNh-12S-H6zSu31TivGwaWbl0I',
-  authDomain: 'anonoymous-messaging-app.firebaseapp.com',
-  databaseURL: 'https://anonoymous-messaging-app.firebaseio.com',
-  projectId: 'anonoymous-messaging-app',
-  storageBucket: 'anonoymous-messaging-app.appspot.com',
-  messagingSenderId: '1007677670774'
+  apiKey: "AIzaSyBH0WBIAdNh-12S-H6zSu31TivGwaWbl0I",
+  authDomain: "anonoymous-messaging-app.firebaseapp.com",
+  databaseURL: "https://anonoymous-messaging-app.firebaseio.com",
+  projectId: "anonoymous-messaging-app",
+  storageBucket: "anonoymous-messaging-app.appspot.com",
+  messagingSenderId: "1007677670774"
 }
 firebase.initializeApp(config)
 firebase.auth().signInAnonymously()
 var createdDate = new Date().getTime()
 // CREATE a new woof in Firebase
 function createWoofInDatabase (woof) {
-  var newWoof = document.getElementById('woof-text').value
-  firebase.database().ref('woofs').push({
+  var newWoof = document.getElementById("woof-text").value
+  firebase.database().ref("woofs").push({
     created_at: createdDate,
     text: newWoof
   })
@@ -27,13 +27,13 @@ function readWoofsInDatabase () {
       addWoofRow(addedSnap.created_at)
       addWoofRow(addedSnap.text)
     })
-  firebase.database().ref('woofs')
+  firebase.database().ref("woofs")
     .on('child_changed', function (updatedSnap) {
       updateWoofRow(updatedSnap.key, updatedSnap.val())
       updateWoofRow(updatedSnap.created_at)
       updateWoofRow(updatedSnap.text)
     })
-  firebase.database().ref('woofs')
+  firebase.database().ref("woofs")
     .on('child_removed', function (deletedSnap) {
       deleteWoofRow(deletedSnap.key, deletedSnap.val())
       deleteWoofRow(deletedSnap.created_at)
@@ -42,14 +42,15 @@ function readWoofsInDatabase () {
 }
 
 function updateWoofInDatabase (woofKey, woofText) {
-  firebase.database().ref('woofs/' + woofKey).set({
+  firebase.database().ref("woofs/" + woofKey).set({
     created_at: createdDate,
     text: woofText
   })
 }
 
 function deleteWoofFromDatabase (woofKey) {
-  firebase.database().ref('woofs/' + woofKey).remove()
-}
+  firebase.database().ref("woofs/" + woofKey).remove()
+}}
+
 // Load all of the data
-readWoofsInDatabase()
+createWoofsInDatabase()
