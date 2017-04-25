a8578ecb7c3869d81b5809790293b6b2321ecd91
jessicalam <j.lam@mail.com>
2017-04-25 13:09:43 -0400

    edited woof file again

--- woofer-db.js
+++ woofer-db.js
@@ -1,4 +1,3 @@
-// TODO Sign into the database anonymously
 // Initialize Firebase
 var config = {
   apiKey: 'AIzaSyBH0WBIAdNh-12S-H6zSu31TivGwaWbl0I',
@@ -10,41 +9,41 @@ var config = {
 }
 firebase.initializeApp(config)
 firebase.auth().signInAnonymously()
-var curDate = new Date().getTime()
+var createdDate = new Date().getTime()
 // CREATE a new woof in Firebase
 function createWoofInDatabase (woof) {
   var newWoof = document.getElementById('woof-text').value
   firebase.database().ref('woofs').push({
-    created_at: curDate,
+    created_at: createdDate,
     text: newWoof
   })
 // READ from Firebase when woofs are added, changed, or removed
 // Write a function for each 'on' method and call addWoofRow,
 // updateWoofRow, and deleteWoofRow to update the page.
-/function readWoofsInDatabase () {
+function readWoofsInDatabase () {
   firebase.database().ref('woofs')
-    .on('child_added', function (addSnap) {
-      addWoofRow(addSnap.key, addSnap.val())
-      addWoofRow(addSnap.created_at)
-      addWoofRow(addSnap.text)
+    .on('child_added', function (addedSnap) {
+      addWoofRow(addedSnap.key, addedSnap.val())
+      addWoofRow(addedSnap.created_at)
+      addWoofRow(addedSnap.text)
     })
   firebase.database().ref('woofs')
-    .on('child_changed', function (updateSnap) {
-      updateWoofRow(updateSnap.key, updateSnap.val())
-      updateWoofRow(updateSnap.created_at)
-      updateWoofRow(updateSnap.text)
+    .on('child_changed', function (updatedSnap) {
+      updateWoofRow(updatedSnap.key, updatedSnap.val())
+      updateWoofRow(updatedSnap.created_at)
+      updateWoofRow(updatedSnap.text)
     })
   firebase.database().ref('woofs')
-    .on('child_removed', function (deleteSnap) {
-      deleteWoofRow(deleteSnap.key, deleteSnap.val())
-      deleteWoofRow(deleteSnap.created_at)
-      deleteWoofRow(deleteSnap.text)
+    .on('child_removed', function (deletedSnap) {
+      deleteWoofRow(deletedSnap.key, deletedSnap.val())
+      deleteWoofRow(deletedSnap.created_at)
+      deleteWoofRow(deletedSnap.text)
     })
 }
 
 function updateWoofInDatabase (woofKey, woofText) {
   firebase.database().ref('woofs/' + woofKey).set({
-    created_at: curDate,
+    created_at: createdDate,
     text: woofText
   })
 }
@@ -53,4 +52,4 @@ function deleteWoofFromDatabase (woofKey) {
   firebase.database().ref('woofs/' + woofKey).remove()
 }
 // Load all of the data
-readWoofsInDatabase ()
+readWoofsInDatabase()
